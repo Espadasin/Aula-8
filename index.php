18dd0ff57e5770603530056511aa73a051d41863
Espadasin <103957016+Espadasin@users.noreply.github.com>
2023-09-29 18:11:07 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Site-aula8</title>
+</head>
+<body>
+    
+    <main class="principal">
+<!-- 
+        <form action="mensagem.php" method="post">
+            <label for="Filho1">Insira o nome e idade do primeiro filho</label>
+            <input type="text" name="Filho1" id="irmão1">
+            <input type="number" name="Idade1" id="IdadeF1">
+
+            <label for="Filho2">Insira o nome e idade do segundo filho</label>
+            <input type="text" name="Filho2" id="irmão2">
+            <input type="number" name="Idade2" id="IdadeF2">
+            <input type="submit" value="Enviar">
+        </form> -->
+
+        <!-- <form action="mensagem.php" method="post">
+            <label for="Peso">Insira seu peso</label>
+            <input type="number" name="Peso">
+
+            <label for="Altura">Insira sua altura</label>
+            <input type="number" name="Altura">
+            <input type="submit" value="Enviar">
+        </form>       -->
+
+        <form action="mensagem.php" method="post">
+            1 para depositar, 2 para sacar e 3 para ver o saldo  <input type="number" name="Opção">
+            <input type="number" name="Deposito" placeholder="Deposito">
+            <input type="submit" value="Enviar">
+        
+        </form>  
+
+
+    </main>
+
+
+</body>
+</html>
---
+++ mensagem.php
@@ -0,0 +1,46 @@
+<?php 
+    
+    //Atividade-1
+
+    // $irmão1 = $_POST["Filho1"];
+    // $idade1 = $_POST["Idade1"];
+
+    // $irmão2 = $_POST["Filho2"];
+    // $idade2 = $_POST["Idade2"];
+
+    // if($idade1 > $idade2){
+    //     echo "<h1>O $irmão1 e mais velho que o $irmão2 </h1>";
+    // } else{
+    //     echo"<h1>O $irmão2 e mais velho que o $irmão1 </h1>";
+    // };
+
+    //Atividade-2
+
+    // $altura = $_POST["Altura"];
+    // $peso = $_POST["Peso"];
+    
+    // $imc = $peso / pow($altura, 2);
+
+    // echo "Seu IMC e de $imc";
+
+    // //Atividade-4
+    // $opção = $_POST["Opção"];
+    // $deposito = $_POST["Deposito"];
+    // $conta = 120;
+
+    // switch($opção){
+    //     case $opção == 1:
+    //         $conta += $deposito;
+    //         echo "Foi realizado um deposito de $conta reais em sua conta";
+    //         break;
+    //     case $opção == 2:
+    //         $conta -= $deposito;
+    //         echo "Foi realizado um Saque de $conta reais em sua conta";
+    //         break;
+    //     case $opção == 3:
+    //         echo "O saldo de sua conta é de $conta";
+    //         break;
+    //     default:
+    //         echo "Selecione uma opção valida";
+    // };
+?>
